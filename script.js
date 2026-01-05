6914a550064342f6ca4a36c51bfbd8969d7f1643
Lucas Souza <lucasdelazeris@gmail.com>
2026-01-05 14:27:12 -0300

    implementação da função de colisão

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ const star_rush = new Audio('sounds/star_rush.mp3');
 
 gameMusic.play();
 jumpSound.volume = 0.3;
+
+const isColliding = (a, b) => {
+  const rectA = a.getBoundingClientRect();
+  const rectB = b.getBoundingClientRect();
+
+  return !(
+    rectA.right < rectB.left ||
+    rectA.left > rectB.right ||
+    rectA.bottom < rectB.top ||
+    rectA.top > rectB.bottom
+  );
+};
