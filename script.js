95fd87de9905f8995ab9aa610a6d19e9bfd91a9c
Lucas Souza <lucasdelazeris@gmail.com>
2026-01-05 14:18:01 -0300

    inicialização de seletores DOM e variáveis globais

---
+++ script.js
@@ -0,0 +1,9 @@
+const mario = document.querySelector('.mario');
+const turtle = document.querySelector('.turtle');
+const pipe = document.querySelector('.pipe');
+const restart = document.querySelector('.restart');
+const pointsEl = document.querySelector('.points');
+const star = document.querySelector('.star');
+
+let gameRunning = true;
+let points = 0;
